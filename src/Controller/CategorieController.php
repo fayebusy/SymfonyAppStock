849caf22580e22218e1c0789b1c2be0977c98cc4
fayeyoussou <fayeyousso@gmail.com>
2022-06-03 19:30:45 +0000

    categorielist

--- src/Controller/CategorieController.php
+++ src/Controller/CategorieController.php
@@ -5,14 +5,21 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Doctrine\ORM\EntityManagerInterface;
+use Doctrine\Persistence\ManagerRegistry;
+use App\Repository\CategorieRepository;
 
 class CategorieController extends AbstractController
 {
     #[Route('/categorielist', name: 'app_categorie')]
-    public function index(): Response
+    public function index(CategorieRepository $cr): Response
     {
+        
+        $cat = $cr->findAll();
+        // var_dump($cat);
         return $this->render('categorie/index.html.twig', [
             'controller_name' => 'CategorieController',
+            'categorie'=> $cat
         ]);
     }
 }
